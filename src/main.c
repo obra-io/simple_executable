4edd86e4f43e9579a45c7c7dd16469826708ce7c
Brandon <brandon@obra.io>
2018-05-16 23:00:26 -0700

    work

--- src/main.c
+++ src/main.c
@@ -2,35 +2,17 @@
 
 int main(void)
 {
-    AlesiStatus_t status;
+    ALESI_ENSURE_OK(alesi_config_crossbar(app_crossbar_descs, app_crossbar_descs_cnt));
 
-    status = alesi_config_crossbar(app_crossbar_descs,
-            app_crossbar_descs_cnt);
+    ALESI_ENSURE_OK(alesi_config_bridge(app_bridge_descs, app_bridge_descs_cnt));
 
-    if (status == ALESI_OK)
-    {
-        status = alesi_config_bridge(app_bridge_descs, app_bridge_descs_cnt);
-    }
+    ALESI_ENSURE_OK(alesi_config_j1939(app_j1939_descs, app_j1939_descs_cnt));
 
-    if (status == ALESI_OK)
-    {
-        status = alesi_config_j1939(app_j1939_descs, app_j1939_descs_cnt);
-    }
+    ALESI_ENSURE_OK(alesi_init());
 
-    if (status == ALESI_OK)
-    {
-        status = alesi_init();
-    }
+    ALESI_ENSURE_OK(alesi_new_hsm("hsm_test", hsm_test_init, 5U, 0U));
 
-    if (status == ALESI_OK)
-    {
-        status = alesi_new_hsm("hsm_test", hsm_test_init, 5U, 0U);
-    }
-
-    if (status == ALESI_OK)
-    {
-       alesi_start();
-    }
+    alesi_start();
 
     /* Will only get here if Alesi fails to start */
     while(1);
